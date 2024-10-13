226a739e81ed3f7f0f840e0153f1bca05b5bf7af
DemonExposer <56930597+DemonExposer@users.noreply.github.com>
2024-10-13 16:45:12 +0300

    Send read status for chats when a user's chats are requested

--- Chat.cs
+++ Chat.cs
@@ -4,4 +4,6 @@ public class Chat {
 	public User User1 { get; set; }
 	
 	public User User2 { get; set; }
+	
+	public bool IsRead { get; set; }
 }
--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -31,17 +31,34 @@ public class ChatController : ControllerBase {
 		command.Parameters.AddWithValue("@modulus", modulus);
 		command.Parameters.AddWithValue("@exponent", exponent);
 		
+		SQLiteCommand command2 = connection.CreateCommand();
 		using (SQLiteDataReader reader = command.ExecuteReader()) {
 			while (reader.Read()) {
+				string foreignModulus = (string) reader["modulus"];
+				string foreignExponent = (string) reader["exponent"];
+				
+				command2.CommandText = "SELECT COUNT() AS count FROM messages JOIN users u1 ON receiver = u1.id JOIN users u2 ON sender = u2.id WHERE is_read = 0 AND u1.modulus = @personalModulus AND u1.exponent = @personalExponent AND u2.modulus = @foreignModulus AND u2.exponent = @foreignExponent;";
+				command2.Parameters.AddWithValue("@personalModulus", modulus);
+				command2.Parameters.AddWithValue("@personalExponent", exponent);
+				command2.Parameters.AddWithValue("@foreignModulus", foreignModulus);
+				command2.Parameters.AddWithValue("@foreignExponent", foreignExponent);
+
+				bool isChatRead;
+				using (SQLiteDataReader reader2 = command2.ExecuteReader()) {
+					reader2.Read();
+					isChatRead = (long) reader2["count"] == 0;
+				}
+				
 				result.Add(new Chat {
 					User1 = new User {
 						Modulus = modulus,
 						Exponent = exponent
 					},
 					User2 = new User {
-						Modulus = (string) reader["modulus"],
-						Exponent = (string) reader["exponent"]
-					}
+						Modulus = foreignModulus,
+						Exponent = foreignExponent
+					},
+					IsRead = isChatRead
 				});
 			}
 		}
